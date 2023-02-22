74429c3565ec1568a7822b48c9fb7e90fe7ed58f
Tooboi <japica123@gmail.com>
2023-02-22 02:13:22 -0500

    Added character

--- src/App.js
+++ src/App.js
@@ -1,88 +1,36 @@
-import React, { useRef, useState } from 'react';
+import { MathUtils } from 'three';
+import React, { useRef, useState, Suspense } from 'react';
 import './App.scss';
 
-import { Canvas, useFrame } from '@react-three/fiber';
-import { OrbitControls, Instances, Instance, Environment, Lightformer, AccumulativeShadows, RandomizedLight } from '@react-three/drei'
-function Box(props) {
-  // This reference gives us direct access to the THREE.Mesh object
-  const ref = useRef()
-  // Hold state for hovered and clicked events
-  const [hovered, hover] = useState(false)
-  const [clicked, click] = useState(false)
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (ref.current.rotation.x += delta))
-  // Return the view, these are regular Threejs elements expressed in JSX
+import { Canvas, useFrame, useThree } from '@react-three/fiber';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { OrbitControls, useGLTF, SpotLight, useDepthBuffer, Environment, Reflector } from '@react-three/drei';
+
+import Character3 from './assets/Character3.jsx';
+
+function GroundPlane() {
   return (
-    <mesh
-      {...props}
-      ref={ref}
-      scale={clicked ? 1.5 : 1}
-      onClick={(event) => click(!clicked)}
-      onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}>
-      <sphereGeometry args={[1, 32, 16]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} emissiveIntensity={hovered ? 2 : 0} emissive='gold'/>
+    <mesh receiveShadow rotation={[5, 0, 0]} position={[0, 0, 0]}>
+      <planeBufferGeometry attach="geometry" args={[500, 500]} />
+      <meshStandardMaterial attach="material" color="white" />
     </mesh>
-  )
+  );
 }
-const Grid = ({ number = 23, lineWidth = 0.026, height = 0.5 }) => (
-  // Renders a grid and crosses as instances
-  <Instances position={[0, -1.02, 0]}>
-    <planeGeometry args={[lineWidth, height]} />
-    <meshBasicMaterial color="#999" />
-    {Array.from({ length: number }, (_, y) =>
-      Array.from({ length: number }, (_, x) => (
-        <group key={x + ':' + y} position={[x * 2 - Math.floor(number / 2) * 2, -0.01, y * 2 - Math.floor(number / 2) * 2]}>
-          <Instance rotation={[-Math.PI / 2, 0, 0]} />
-          <Instance rotation={[-Math.PI / 2, 0, Math.PI / 2]} />
-        </group>
-      ))
-    )}
-    <gridHelper args={[100, 100, '#bbb', '#bbb']} position={[0, -0.01, 0]} />
-  </Instances>
-)
+
+
+
 const App = () => {
   return (
-    <Canvas shadows orthographic camera={{ position: [10, 20, 20], zoom: 100 }} gl={{ preserveDrawingBuffer: true }}>
-      <ambientLight intensity={0.1} />
-      {/* <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} /> */}
-      {/* <pointLight position={[-10, -10, -10]} /> */}
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
-      <OrbitControls
-        autoRotateSpeed={-0.1}
-        zoomSpeed={0.25}
-        minZoom={40}
-        maxZoom={140}
-        enablePan={false}
-        dampingFactor={0.05}
-        minPolarAngle={Math.PI / 3}
-        maxPolarAngle={Math.PI / 3}
-      />
-            <Environment resolution={32}>
-        <group rotation={[-Math.PI / 4, -0.3, 0]}>
-          <Lightformer intensity={20} rotation-x={Math.PI / 2} position={[0, 5, -9]} scale={[10, 10, 1]} />
-          <Lightformer intensity={2} rotation-y={Math.PI / 2} position={[-5, 1, -1]} scale={[10, 2, 1]} />
-          <Lightformer intensity={2} rotation-y={Math.PI / 2} position={[-5, -1, -1]} scale={[10, 2, 1]} />
-          <Lightformer intensity={2} rotation-y={-Math.PI / 2} position={[10, 1, 0]} scale={[20, 2, 1]} />
-          <Lightformer type="ring" intensity={2} rotation-y={Math.PI / 2} position={[-0.1, -1, -5]} scale={10} />
-        </group>
-      </Environment>
-      <AccumulativeShadows
-        temporal
-        frames={100}
-        color={'#1c1c1c'}
-        colorBlend={5}
-        toneMapped={true}
-        alphaTest={0.9}
-        opacity={1}
-        scale={30}
-        position={[0, -1.01, 0]}>
-        <RandomizedLight amount={4} radius={10} ambient={0.5} intensity={1} position={[0, 10, -10]} size={15} mapSize={1024} bias={0.0001} />
-      </AccumulativeShadows>
-      <Grid />
+    <Canvas concurrent shadows camera={{ fov: 20, far: 100, position: [0, -10, 50], zoom: 1.5 }}>
+      {/* <ambientLight intensity={0.1} /> */}
+      <OrbitControls />
+      <Suspense fallback={null}>
+        <Environment preset="sunset" />
+        <Character3 />
+        <GroundPlane />
+      </Suspense>
     </Canvas>
-  )
-}
+  );
+};
 
 export default App;
